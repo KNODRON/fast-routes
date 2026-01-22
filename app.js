ef085aabed7fdfbcc2a24327cb33b669bf1d6cb9
KNODRON <juan.jimenezv08@gmail.com>
2026-01-22 10:27:48 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,65 +1,57 @@
-
 // app.js - Controla la carga y visualización de comisarías y subunidades
 const map = L.map('map').setView([-33.447867, -70.657181], 12);
 
 // Capa base
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors'
+  attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
 let comisariaLayer = L.layerGroup().addTo(map);
 let subunidadLayer = L.layerGroup().addTo(map);
 
 document.getElementById('btnComisarias').addEventListener('click', () => {
-    fetch('unidades_carabineros.json')
-        .then(res => res.json())
-        .then(data => {
-            comisariaLayer.clearLayers();
-            subunidadLayer.clearLayers();
+  fetch('unidades_carabineros.json')
+    .then(res => res.json())
+    .then(data => {
+      comisariaLayer.clearLayers();
+      subunidadLayer.clearLayers();
 
-            data.forEach(com => {
-                const [lat, lon] = com.coords.split(',').map(Number);
+      data.forEach(com => {
+        const [lat, lon] = com.coords.split(',').map(Number);
 
-                const marker = L.marker([lat, lon], {
-                    icon: L.icon({
-                        iconUrl: 'https://cdn-icons-png.flaticon.com/128/4875/4875484.png',
-                        iconSize: [30, 30]
-                    })
-                }).addTo(comisariaLayer);
+        const marker = L.marker([lat, lon], {
+          icon: L.icon({
+            iconUrl: 'https://cdn-icons-png.flaticon.com/128/4875/4875484.png',
+            iconSize: [30, 30]
+          })
+        }).addTo(comisariaLayer);
 
-                const popup = document.createElement('div');
-                popup.innerHTML = '<strong>' + com.nombre + '</strong><br>';
+        const popup = document.createElement('div');
+        popup.innerHTML = '<strong>' + com.nombre + '</strong><br>';
 
-                const navBtn = document.createElement('button');
-                navBtn.textContent = 'Navegar';
-                navBtn.onclick = () => {
-                    const gmaps = `https://www.google.com/maps/search/?api=1&query=${lat},${lon}`;
-                    window.open(gmaps, '_blank');
-                };
-                popup.appendChild(navBtn);
+        if (com.subunidades && com.subunidades.length > 0) {
+          const ul = document.createElement('ul');
+          com.subunidades.forEach(sub => {
+            const li = document.createElement('li');
+            const subCoords = sub.coords.split(',').map(Number);
+            li.innerHTML = `
+              <a href="#" onclick="map.setView([${subCoords}], 17); return false;">
+                ${sub.nombre}
+              </a>
+            `;
+            ul.appendChild(li);
 
-                if (com.subunidades && com.subunidades.length > 0) {
-                    const subBtn = document.createElement('button');
-                    subBtn.textContent = 'Mostrar subunidades';
-                    subBtn.style.marginLeft = '10px';
-                    subBtn.onclick = () => {
-                        subunidadLayer.clearLayers();
-                        com.subunidades.forEach(sub => {
-                            const [subLat, subLon] = sub.coords.split(',').map(Number);
-                            L.marker([subLat, subLon], {
-                                icon: L.icon({
-                                    iconUrl: 'https://cdn-icons-png.flaticon.com/128/3038/3038789.png',
-                                    iconSize: [26, 26]
-                                })
-                            })
-                                .bindPopup(`<strong>${sub.nombre}</strong>`)
-                                .addTo(subunidadLayer);
-                        });
-                    };
-                    popup.appendChild(subBtn);
-                }
+            L.marker(subCoords, {
+              icon: L.icon({
+                iconUrl: 'https://cdn-icons-png.flaticon.com/128/685/685815.png',
+                iconSize: [24, 24]
+              })
+            }).addTo(subunidadLayer);
+          });
+          popup.appendChild(ul);
+        }
 
-                marker.bindPopup(popup);
-            });
-        });
+        marker.bindPopup(popup);
+      });
+    });
 });
